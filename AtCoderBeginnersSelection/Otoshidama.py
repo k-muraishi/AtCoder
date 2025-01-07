1066904444ba541bf93bad52cd44fcd11d98035f
“Kohei <muraishi634@gmail.com>
2025-01-08 04:23:18 +0900

    fix

--- AtCoderBeginnersSelection/Otoshidama.py
+++ AtCoderBeginnersSelection/Otoshidama.py
@@ -5,16 +5,15 @@ y = a[1]
 cnt = 0
 
 for i in range(0, n+1):
-    if cnt != 0:
-        break
     for j in range(0, n+1):
-        if cnt != 0:
-            break
         k = n - i - j # n == (i + j + k)になるので、iとjが決まればkも決まる
         if k >=0 and n == (i + j + k) and y == (10000*i + 5000*j + 1000*k):
             print(f'{i} {j} {k}')
             cnt += 1
             break
+    else:
+        continue
+    break
 
 if cnt == 0:
     print('-1 -1 -1')
