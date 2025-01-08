b12abd134f8618d3314f81f4e436c3aaa2245895
“Kohei <muraishi634@gmail.com>
2025-01-09 07:31:27 +0900

    fix

--- AtCoderBeginnersSelection/白昼夢.py
+++ AtCoderBeginnersSelection/白昼夢.py
@@ -1,36 +1,52 @@
 import sys
 
-lint = str(input())
+line = str(input())
+divide = ['dream','dreamer','erase','eraser']
 
-if len(list(lint)) <= 4:
-    print('NO')
-    sys.exit(0)
+# 入力文字列、定数をそれぞれ左右反対にする
+line = line[::-1]
+for i in range(len(divide)):
+    divide[i] = divide[i][::-1]
 
-flg = True
 
-while flg:
-    if lint[0:5] == 'dream' or lint[0:5] == 'erase':
-        lint = lint[5:]
+
+
+
+
+
+
+
+
+
+# if len(list(lint)) <= 4:
+#     print('NO')
+#     sys.exit(0)
+
+# flg = True
+
+# while flg:
+#     if lint[0:5] == 'dream' or lint[0:5] == 'erase':
+#         lint = lint[5:]
     
-    if lint == '':
-        print('YES')
-        flg = False
-        break
+#     if lint == '':
+#         print('YES')
+#         flg = False
+#         break
 
-    if len(list(lint)) <= 4:
-        print('NO')
-        flg = False
-        break
+#     if len(list(lint)) <= 4:
+#         print('NO')
+#         flg = False
+#         break
 
-    if lint[0:6] == 'dreamer' or lint[0:6] == 'eraser':
-        lint = lint[6:]
+#     if lint[0:6] == 'dreamer' or lint[0:6] == 'eraser':
+#         lint = lint[6:]
 
-    if lint == '':
-        print('YES')
-        flg = False
-        break
+#     if lint == '':
+#         print('YES')
+#         flg = False
+#         break
 
-    if len(list(lint)) <= 4:
-        print('NO')
-        flg = False
-        break
+#     if len(list(lint)) <= 4:
+#         print('NO')
+#         flg = False
+#         break
