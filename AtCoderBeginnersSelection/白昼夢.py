2d73ff80628fcf428b50734b24b9c1c469fae966
“Kohei <muraishi634@gmail.com>
2025-01-10 06:59:11 +0900

    fix

--- AtCoderBeginnersSelection/白昼夢.py
+++ AtCoderBeginnersSelection/白昼夢.py
@@ -4,49 +4,29 @@ line = str(input())
 divide = ['dream','dreamer','erase','eraser']
 
 # 入力文字列、定数をそれぞれ左右反対にする
+# すべて文字を抽出できる文字列が与えられたら後ろからでも最終的にすべて抽出できる
+# 逆の場合、後ろからだと抽出できず最初のループでflg2がfalseのままとなる
+
 line = line[::-1]
 for i in range(len(divide)):
     divide[i] = divide[i][::-1]
 
+flg = True
 
+while(flg):
+    flg2 = False
+    for i in range(len(divide)):
+        d = divide[i]
+        dline = line[0:len(d)]
+        if line[0:len(d)] == d:
+            line = line[len(d):]
+            flg2 = True
+            break
 
+    if flg2 == False or len(line) == 0:
+        flg = False
 
-
-
-
-
-
-
-
-# if len(list(lint)) <= 4:
-#     print('NO')
-#     sys.exit(0)
-
-# flg = True
-
-# while flg:
-#     if lint[0:5] == 'dream' or lint[0:5] == 'erase':
-#         lint = lint[5:]
-    
-#     if lint == '':
-#         print('YES')
-#         flg = False
-#         break
-
-#     if len(list(lint)) <= 4:
-#         print('NO')
-#         flg = False
-#         break
-
-#     if lint[0:6] == 'dreamer' or lint[0:6] == 'eraser':
-#         lint = lint[6:]
-
-#     if lint == '':
-#         print('YES')
-#         flg = False
-#         break
-
-#     if len(list(lint)) <= 4:
-#         print('NO')
-#         flg = False
-#         break
+if len(line) == 0:
+    print('YES')
+else:
+    print('NO')
